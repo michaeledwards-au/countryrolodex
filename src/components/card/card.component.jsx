d1c7cee9a50de49361ca1bae02de96b31d9defad
Michael Edwards <me@michaeledwards.me>
2023-11-24 21:56:03 +1100

    Update card.component.jsx to change from svg to png.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ const Card = ({country, useKey}) => {
 
   return (
     <div className='card'>
-      <img alt={flags.alt} src={flags.svg} />
+      <img alt={flags.alt} src={flags.png} />
       <h2>{name.common}</h2>
       <p>Population: {population}</p>
     </div>
